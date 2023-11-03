2dfa1d5c46b9bcb27e037eb591e09b6d61aba619
Peter Lejon <peter.lejon@voady.com>
2023-11-03 14:09:13 +0100

    first commit

---
+++ src/test/java/se/voady/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package se.voady;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
